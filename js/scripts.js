23ab0309a6b86a8869ea330e15bb686bf03e095a
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-01-13 10:16:55 +0300

    add overlay mouseover in JS

--- js/scripts.js
+++ js/scripts.js
@@ -31,13 +31,63 @@ $(document).ready(function() {
         });
             
 });
-$(document).ready(function() {
-$('work1').mouseover(function (){
-    $('#overlay').show();
-}).mouseout(function (){
-    $('#overlay').hide();
+
+$(".work1").mouseover(function() {
+    $("#overlay").show();
 });
+$(".work1").mouseout(function() {
+    $("#overlay").hide();
 });
+
+$(".work2").mouseover(function() {
+    $("#overlay2").show();
+});
+$(".work2").mouseout(function() {
+    $("#overlay2").hide();
+});
+
+$(".work3").mouseover(function() {
+    $("#overlay3").show();
+});
+$(".work3").mouseout(function() {
+    $("#overlay3").hide();
+});
+
+$(".work4").mouseover(function() {
+    $("#overlay4").show();
+});
+$(".work4").mouseout(function() {
+    $("#overlay4").hide();
+});
+
+$(".work5").mouseover(function() {
+    $("#overlay5").show();
+});
+$(".work5").mouseout(function() {
+    $("#overlay5").hide();
+});
+
+$(".work6").mouseover(function() {
+    $("#overlay6").show();
+});
+$(".work6").mouseout(function() {
+    $("#overlay6").hide();
+});
+
+$(".work7").mouseover(function() {
+    $("#overlay7").show();
+});
+$(".work7").mouseout(function() {
+    $("#overlay7").hide();
+});
+
+$(".work8").mouseover(function() {
+    $("#overlay8").show();
+});
+$(".work8").mouseout(function() {
+    $("#overlay8").hide();
+});
+
 
 $("form#my-form").submit(function (event){
     event.preventDefault();
